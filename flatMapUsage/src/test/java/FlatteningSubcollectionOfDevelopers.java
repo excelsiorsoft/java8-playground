0e3a3336550611645374e1cc7b9153d3377b41ae
sleyzerzon <senya22@yahoo.com>
2017-12-17 13:05:36 -0500

    renaming

--- flatMapUsage/src/test/java/FlatteningSubcollectionOfDevelopers.java
+++ flatMapUsage/src/test/java/FlatteningSubcollectionOfDevelopers.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 
 
 
-public class FlatMapTest {
+public class FlatteningSubcollectionOfDevelopers {
 
     private static List<Developer> team;
     private static Developer polyglot;

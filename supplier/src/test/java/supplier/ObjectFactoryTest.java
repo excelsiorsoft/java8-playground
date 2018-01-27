100145b3894c68607037b04a3834a9629f8eab8a
sleyzerzon <senya22@yahoo.com>
2018-01-27 17:27:23 -0500

    object creation via non-default constructor

---
+++ supplier/src/test/java/supplier/ObjectFactoryTest.java
@@ -0,0 +1,34 @@
+package supplier;
+
+import static java.util.Arrays.asList;
+
+import java.awt.Choice;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+import org.junit.Test;
+
+import supplier.Shape.Circle;
+import supplier.Shape.Rectangle;
+import supplier.Shape.Square;
+
+public class ObjectFactoryTest {
+
+	ObjectFactory cut = new ObjectFactory();
+	
+	@Test
+	public void construction() {
+		
+		Person personSupplier = ObjectFactory.bind(Person::new, "Simeon").get();
+		System.out.println(personSupplier);
+
+	}
+	
+	
+	
+
+
+}

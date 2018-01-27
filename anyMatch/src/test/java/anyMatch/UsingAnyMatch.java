94b9d1d4aac84809f419c8e9d401bb0b38cf1cf8
sleyzerzon <senya22@yahoo.com>
2018-01-27 11:23:51 -0500

    ignore mod

--- anyMatch/src/test/java/anyMatch/UsingAnyMatch.java
+++ anyMatch/src/test/java/anyMatch/UsingAnyMatch.java
@@ -2,12 +2,14 @@ package anyMatch;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+//import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.BeforeAll;
+//import org.junit.jupiter.api.Test;
 
 
 
@@ -16,7 +18,7 @@ public class UsingAnyMatch {
 
     private static  List<String> stringArrayList;
 
-    @BeforeAll
+    @BeforeClass
     public static void setUp() {
         stringArrayList = new ArrayList<>();
         stringArrayList.add("string1 is present");
@@ -30,6 +32,16 @@ public class UsingAnyMatch {
         boolean found = stringArrayList.stream().anyMatch(s -> s.contains("string2"));
         System.out.println("found: " + found);
     }
+
+
+    @Test
+    public void anotherWay() {
+        stringArrayList.stream()
+                .filter(e -> e.contains("string2"))
+                .findFirst()
+                .ifPresent(e -> System.out.println("Match found"));
+    }
+
 
 
 }

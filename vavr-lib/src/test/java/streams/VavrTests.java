efb319b7ea9d1bec890b38033944ada4642b9d1c
sleyzerzon <senya22@yahoo.com>
2018-02-03 15:53:39 -0500

    vavr24

--- vavr-lib/src/test/java/streams/VavrTests.java
+++ vavr-lib/src/test/java/streams/VavrTests.java
@@ -185,8 +185,8 @@ public class VavrTests {
 		assertThat(that2._1).isEqualTo("vavr");
 		assertThat(that2._2).isEqualTo(1);
 		
-		String r = java8.apply((s, i) -> s.substring(2) + "vr"+ i / 8);
-		assertThat(r).hasToString("vavr1");
+		String r = java8.apply((s, i) -> s.substring(2) + "vr"+ i*3);
+		assertThat(r).hasToString("vavr24");
 	}
 
 }

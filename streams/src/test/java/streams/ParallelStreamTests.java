0663bae1b00a0ca5d3e550558190d1f914f2ecc2
sleyzerzon <senya22@yahoo.com>
2018-02-12 19:12:09 -0500

    handling race conditons

--- streams/src/test/java/streams/ParallelStreamTests.java
+++ streams/src/test/java/streams/ParallelStreamTests.java
@@ -2,6 +2,9 @@ package streams;
 
 
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.stream.Stream;
 
 import org.junit.Test;
@@ -34,6 +37,36 @@ public class ParallelStreamTests {
 		.limit(6)
 		.peek(e -> System.out.println(e+" is processed by "+ Thread.currentThread().getName())) //
 		.forEach(System.out::println);
+	}
+	
+	
+	
+	@Test
+	public void raceConditions() throws Exception {
+		
+		List<String> strings = new ArrayList<>(); //not thread-safe
+		
+		Stream.iterate("+", s -> s+"+")
+		.parallel()
+		.limit(10000)
+		//.peek(e -> System.out.println(e+" is processed by "+ Thread.currentThread().getName()))
+		.forEach(e -> strings.add(e));
+		
+		System.out.println("# of elems: "+strings.size());
+	}
+	
+	@Test
+	public void threadSafe() throws Exception {
+		
+		List<String> strings = new CopyOnWriteArrayList<>(); //concurrent-aware
+		
+		Stream.iterate("+", s -> s+"+")
+		.parallel()
+		.limit(10000)
+		//.peek(e -> System.out.println(e+" is processed by "+ Thread.currentThread().getName()))
+		.forEach(e -> strings.add(e));
+		
+		System.out.println("# of elems: "+strings.size());
 	}
 	
 

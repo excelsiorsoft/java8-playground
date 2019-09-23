2a92fd606eddcde23175e636a8021d011baf6df8
sleyzerzon <senya22@yahoo.com>
2019-09-22 21:27:57 -0400

    playing with radioactive project

--- spider/src/main/java/com/excelsiorsoft/spider/bean/MaybeSetter.java
+++
@@ -1,9 +0,0 @@
-package com.excelsiorsoft.spider.bean;
-
-import java.util.Optional;
-
-public interface MaybeSetter<T, V> extends Setter<T, Optional<V>> {
-    default Setter<T, V> assertNotNull() {
-        return (t, v) -> set(t, Optional.of(v));
-    }
-}

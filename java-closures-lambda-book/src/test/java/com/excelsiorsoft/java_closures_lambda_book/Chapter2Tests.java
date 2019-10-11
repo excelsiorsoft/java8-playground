72ff462c37f53b4a07b78496941565cf6e0dd7e9
sleyzerzon <senya22@yahoo.com>
2019-10-11 12:50:55 -0400

    java closures & lambda book

--- java-closures-lambda-book/src/test/java/com/excelsiorsoft/java_closures_lambda_book/Chapter2Tests.java
+++ java-closures-lambda-book/src/test/java/com/excelsiorsoft/java_closures_lambda_book/Chapter2Tests.java
@@ -2,14 +2,33 @@ package com.excelsiorsoft.java_closures_lambda_book;
 
 import java.util.Arrays;
 import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 import org.junit.Test;
 
 
 public class Chapter2Tests {
 
-
+	public static void greetFolks(Function<String, String> greeter) {
+		for (String name: Arrays.asList("Alice", "Bob", "Cathy")) {
+			System.out.println(greeter.apply(name));
+		}
+	}
+	
+	public static <T,U,V> Function<T,Function<U,V>> curry(BiFunction<T,U,V> bif){
+		return t -> (u -> bif.apply(t,u));
+	}
+	
+	@Test
+	public void currying() {
+		BiFunction<String, String, String> concat = (a,b) -> a + b;
+		Function<String, Function<String, String>> curriedConcat = curry(concat);
+		for(String greetings: Arrays.asList("Hello", "Guten Tag", "Bonjour")) {
+			greetFolks(curriedConcat.apply(greetings + ", "));
+		}
+	}
 	
 	@Test
 	public void lambdasWithNoReturnValue() {

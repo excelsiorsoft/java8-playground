cf65a5516616e17553fa14ea1138e289c1bb1fab
sleyzerzon <senya22@yahoo.com>
2020-02-18 22:53:11 -0500

    added after stuff

---
+++ combinators-pattern/src/main/java/com/excelsiorsoft/combinators/AfterExample.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+package com.excelsiorsoft.combinators;
+
+/**
+ * @author Simeon
+ *
+ */
+import java.math.BigDecimal;
+import java.util.function.Function;
+
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+
+@FunctionalInterface
+interface After<T, R>
+        extends Function<Function<T, R>,
+                Function<BiConsumer<T, R>,
+                Function<T, R>>> {
+
+    static <T, R> After<T, R> create() {
+        return function -> after -> argument -> {
+            R result = function.apply(argument);
+            after.accept(argument, result);
+            return result;
+        };
+    }
+
+    static <T, R> Function<T, R> decorate(
+            Function<T, R> function,
+            BiConsumer<T, R> after) {
+        return After.<T, R>create().apply(function).apply(after);
+    }
+}
+
+public class AfterExample {
+
+    void demo() {
+        System.out.println("---------------------------------");
+        System.out.println("Starting AFTER combinator demo...");
+        System.out.println("---------------------------------");
+
+        Function<BigDecimal, String> addTaxDecorated =
+                After.decorate(this::addTax, this::after);
+
+        String result = addTaxDecorated.apply(new BigDecimal("1000"));
+
+        System.out.println("Done - Result is " + result);
+        System.out.println();
+    }
+
+    private String addTax(BigDecimal amount) {
+        System.out.println("Adding heavy taxes to poor citizen...");
+        return "$" + amount.multiply(new BigDecimal("1.22"));
+    }
+
+    private void after(BigDecimal argument, String result) {
+        System.out.println("AFTER: Argument was " + argument + ", Result is " + result);
+    }
+}
---
+++ combinators-pattern/src/test/java/com/excelsiorsoft/combinators/AfterExampleTest.java
@@ -0,0 +1,17 @@
+package com.excelsiorsoft.combinators;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AfterExampleTest {
+
+private AfterExample after;
+	
+	@Test
+	public void testAfter() {
+		after = new AfterExample();
+		after.demo();
+	}
+
+}
